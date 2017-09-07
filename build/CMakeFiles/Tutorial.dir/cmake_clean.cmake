ef5d6bffd2c77e7b449a301c0d5da8ba08b8d7d9
Ubuntu 14.04 <nignatyuk@gmail.com>
2017-09-07 04:42:33 -0700

    These are the files of my finished PA1.

---
+++ build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  "Tutorial.pdb"
+  "Tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Tutorial_SUCCESSFUL.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial_SUCCESSFUL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Tutorial_SUCCESSFUL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
