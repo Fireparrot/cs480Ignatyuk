f82f02ae77eb9fec7cb5d1bf8392594dacc341bd
Ubuntu 14.04 <nignatyuk@gmail.com>
2017-09-13 04:07:45 -0700

    PA2

---
+++ CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fireparrot/Desktop/CS480_2/src/camera.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "/home/fireparrot/Desktop/CS480_2/src/engine.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "/home/fireparrot/Desktop/CS480_2/src/graphics.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "/home/fireparrot/Desktop/CS480_2/src/main.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "/home/fireparrot/Desktop/CS480_2/src/object.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "/home/fireparrot/Desktop/CS480_2/src/shader.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "/home/fireparrot/Desktop/CS480_2/src/window.cpp" "/home/fireparrot/Desktop/CS480_2/build/CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNIX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
