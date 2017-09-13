e8eb47a29dbfdcc16152141711feaf2f94b7e878
Ubuntu 14.04 <nignatyuk@gmail.com>
2017-09-13 04:20:58 -0700

    PA2

---
+++ include/object.h
@@ -0,0 +1,28 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt, float rotation);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle1;
+    float angle2;
+};
+
+#endif /* OBJECT_H */
